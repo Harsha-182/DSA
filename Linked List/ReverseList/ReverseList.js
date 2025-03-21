6e8d2c9c2e76535cc162f703595cfaf932c01b63
Harsha <97773381+Harsha-182@users.noreply.github.com>
2025-03-21 18:20:41 +0530

    ReverseLinkedlist

---
+++ Linked List/ReverseList/ReverseList.js
@@ -0,0 +1,31 @@
+// Approach to Reverse a Linked List
+// Given a singly linked list, we need to reverse it in-place (i.e., without using extra space).
+
+// Approach:-
+// 1.  Iterative Method (Efficient)
+// prev (initially null)
+// current (initially head)
+// next (to store current.next before modifying it)
+
+// Traverse the linked list while current is not null.
+// Store the next node (next = current.next).
+// Reverse the current.next pointer to point to prev (current.next = prev).
+// Move prev and current one step forward.
+// After the loop, prev will be the new head of the reversed list.
+
+function reverseList(){
+    let prev = null;
+    let current = this.head;
+    let next = null;
+    
+    while(current){
+        next = current.next;  // Store next node
+        current.next = prev;  // Reverse the link
+        prev = current;       // Move prev to current node
+        current = next;       // Move current to next node
+    }
+  
+    this.head = prev;  // Update head to new first node
+
+    return this.head;
+}
